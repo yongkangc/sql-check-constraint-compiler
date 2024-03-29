53e7d283850f487932f45d9bc00c931f9ca9126b
Chia Yong Kang <chiayongkang@hotmail.com>
2024-03-29 14:42:40 +0800

    added docs to init.py

---
+++ pgcheck/__init__.py
@@ -0,0 +1,8 @@
+"""
+pgCheck - PostgreSQL Check Constraint Compiler
+
+This package provides a command-line interface (CLI) tool to facilitate PostgreSQL users
+in managing CHECK constraints by automatically translating them into triggers and stored
+functions. It also includes a performance evaluation component to assess the efficiency
+of the translated constraints.
+"""
